d0684b78679916b25f86e3cc516fcce4e48dc005
DmtrBonapart <bonapartdmitriy@gmail.com>
2025-12-22 11:26:04 +0900

    day 5 рекурсивные функции

--- 5day/RecursiveFunc.py
+++ 5day/RecursiveFunc.py
@@ -52,61 +52,62 @@
 
 
 #FinalBoss
-# def solve(maze, row=None, col=None, path=None):
-#     if row is None:
-#         # ищу старт
-#         for i in range(len(maze)):
-#             for j in range(len(maze[0])):
-#                 if maze[i][j] == 's':
-#                     # ищу пути, добавляя старт в начало
-#                     resultat = solve(maze, i, j, [])
-#                     #стартовая точка в каждом пути
-#                     for marshrut in resultat:
-#                         marshrut.insert(0, f's({i},{j})')
-#                     return resultat
-#         return []
-#
-#     #если робот дошёл до x - добавляем этот путь в список правильных путей
-#     if maze[row][col] == 'x':
-#         return [path] if path else [[]]
-#
-#     orig = maze[row][col] #сохраняю значение клетки
-#     maze[row] = maze[row][:col] + '#' + maze[row][col + 1:] #помечаю текущую клетку стеной(посещённая)
-#
-#     vse_puti = []
-#     napravleniya = [(-1, 0, 'Вверх'), (1, 0, 'Вниз'), (0, -1, 'Влево'), (0, 1, 'Вправо')]
-#
-#     for x, y, napravlenie in napravleniya:
-#         new_row, new_col = row + x, col + y
-#
-#         #проверяю могу ли пойти на клетку
-#         if (0 <= new_row < len(maze) and 0 <= new_col < len(maze[0]) and
-#                 maze[new_row][new_col] != '#'):
-#             #проверяем ещё дополнительный путь
-#             dop_puti = solve(maze, new_row, new_col, path + [napravlenie])
-#             vse_puti.extend(dop_puti) #добавляю доп пути в список путей
-#
-#     #возвращаю ориг значение клетки, чтобы пройти другим путём
-#     # maze[row] = maze[row][:col] + orig + maze[row][col + 1:]
-#     return vse_puti
-#
-# maze = [
-#     's----',
-#     '##---',
-#     '---##',
-#     '----x'
-# ]
-#
-# maze2 = [
-#     '--s--',
-#     '##---',
-#     '---##',
-#     '--x--'
-# ]
-#
-# itog = solve(maze)
-# for i in itog:
-#     print(i)
+def solve(maze, row=None, col=None, path=None):
+    if row is None:
+        # ищу старт
+        for i in range(len(maze)):
+            for j in range(len(maze[0])):
+                if maze[i][j] == 's':
+                    # ищу пути, добавляя старт в начало
+                    resultat = solve(maze, i, j, [])
+                    #стартовая точка в каждом пути
+                    for marshrut in resultat:
+                        marshrut.insert(0, f's({i},{j})')
+                    return resultat
+        return []
+
+    #если робот дошёл до x - добавляем этот путь в список правильных путей
+    if maze[row][col] == 'x':
+        return [path] if path else [[]]
+
+    orig = maze[row][col] #сохраняю значение клетки
+    maze[row] = maze[row][:col] + '#' + maze[row][col + 1:] #помечаю текущую клетку стеной(посещённая)
+
+    vse_puti = []
+    napravleniya = [(-1, 0, 'Вверх'), (1, 0, 'Вниз'), (0, -1, 'Влево'), (0, 1, 'Вправо')]
+
+    for x, y, napravlenie in napravleniya:
+        new_row, new_col = row + x, col + y
+
+        #проверяю могу ли пойти на клетку
+        if (0 <= new_row < len(maze) and 0 <= new_col < len(maze[0]) and
+                maze[new_row][new_col] != '#'):
+            #проверяем ещё дополнительный путь
+            dop_puti = solve(maze, new_row, new_col, path + [napravlenie])
+            vse_puti.extend(dop_puti) #добавляю доп пути в список путей
+
+    #возвращаю ориг значение клетки, чтобы пройти другим путём
+    # maze[row] = maze[row][:col] + orig + maze[row][col + 1:]
+    return vse_puti
+
+maze = [
+    's----',
+    '##---',
+    '---##',
+    '----x'
+]
+
+maze2 = [
+    '--s--',
+    '##---',
+    '---##',
+    '--x--'
+]
+
+itog = solve(maze)
+itog = solve(maze2)
+for i in itog:
+    print(i)
 
 
 
